48f1282f84ab36cdcfef2f513ef3443d35a2eaf1
Eric Wagner <eric.wagner@geneticfinance.com>
2024-09-04 14:06:34 -0700

    Revove

--- temp.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-curl -X POST https://cflare-worker.eric-04e.workers.dev/ \
--H "Authorization: Bearer test-token" \
--H "Content-Type: application/json" \
--d '{"cgen": "yes", "ai": "llm", "cgenmessage": "This works!"}'
-echo ""
