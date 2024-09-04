a0e889fa269df7cb43a157676ca6843ac22e4d87
Eric Wagner <eric.wagner@geneticfinance.com>
2024-09-04 14:09:41 -0700

    Add CLI check

--- api_test.sh
+++ api_test.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
+# Get the URL 
 url=$1
 
+# Check if the first argument (url) is empty
+if [ -z "$url" ]; then
+  echo "Error: URL is missing."
+  echo "Usage: ./script.sh <url>"
+  exit 1
+fi
+
+# Run the producer
 curl -X POST $url \
 -H "Authorization: Bearer test-token" \
 -H "Content-Type: application/json" \
